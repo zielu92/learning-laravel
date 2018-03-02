f2559ed48053aeb1fd23174ad0236a79b99d9b7f
zielu92 <zielu0@gmail.com>
2018-03-02 11:48:13 +0100

    added files

---
+++ app/Http/routes.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+
+    return view('welcome');
+
+});
+
+Route::get('/about', function () {
+
+    return "about page";
+
+});
+
+Route::get('/contact', function () {
+
+    return "contact";
+
+});
+
+Route::get('/post/{id}/{name}', function($id, $name) {
+
+    return "This is post number ". $id . " ". $name;
+
+});
+
+Route::group(['middleware' => ['web']], function () {
+
+
+});
